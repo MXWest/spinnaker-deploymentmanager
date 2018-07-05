46f3509a7673d8719556eb1b02dfd85022af8d9d
Mike West <mwest@urbn.com>
2018-07-05 16:37:09 -0400

    Fix incorrect . of env vars

--- tutorial/spin-destroy.sh
+++ tutorial/spin-destroy.sh
@@ -2,7 +2,7 @@
 #
 #
 set -x
-. ~/bin/spin-env.sh
+. spin-env.sh
 export SPINNAKER_VM=$(gcloud compute instances list --filter="name~${DEPLOYMENT_NAME}-spinnaker.+" --uri)
 gcloud compute ssh ${SPINNAKER_VM} -- sudo service front50 stop
 gsutil rm -r gs://spinnaker-${GOOGLE_PROJECT}-${DEPLOYMENT_NAME}/front50
